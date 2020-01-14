3692b6fb3add9378e8d3c68c415d2ced7162514f
Olivier Miserez <olivierm@avio.aero>
2020-01-14 16:23:03 +0100

    Initial Commit

---
+++ Organizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Organizer
+//
+//  Created by Olivier Miserez on 14/01/2020.
+//  Copyright © 2020 Olivier Miserez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
