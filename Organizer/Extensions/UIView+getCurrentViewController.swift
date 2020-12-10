b669086db1551b5ded236fbc160778d7eea58c29
Olivier Miserez <olivierm@avio.aero>
2020-12-10 10:04:42 +0100

    Change UIApplication rootController to UIApplication.connectedScenes for ios13

--- Organizer/Extensions/UIView+getCurrentViewController.swift
+++ Organizer/Extensions/UIView+getCurrentViewController.swift
@@ -12,8 +12,15 @@ import UIKit
 extension UIView {
     // MARK: get Current ViewController
     func getCurrentViewController() -> UIViewController? {
-        if let rootController = UIApplication.shared.keyWindow?.rootViewController {
-            var currentController: UIViewController! = rootController
+        let currentWindow: UIWindow? = UIApplication.shared.connectedScenes
+            .filter({$0.activationState == .foregroundActive})
+            .map({$0 as? UIWindowScene})
+            .compactMap({$0})
+            .first?.windows
+            .filter({$0.isKeyWindow}).first
+        
+        if let sceneRootController = currentWindow {
+            var currentController: UIViewController! = sceneRootController.rootViewController
             while currentController.presentedViewController != nil {
                 currentController = currentController.presentedViewController
             }
