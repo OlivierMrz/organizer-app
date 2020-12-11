d05bffcf9dc3151bc309745873e878c661cf9fb7
Olivier Miserez <olivierm@avio.aero>
2020-12-12 00:23:45 +0100

    Change type in Appdelegate

--- Organizer/Classes/Application/AppDelegate.swift
+++ Organizer/Classes/Application/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print("🚨 DatabsasePath: ", FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last ?? "Not Found!")
+        print("🚨 DatabasePath: ", FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last ?? "Not Found!")
 
         window = UIWindow(frame: UIScreen.main.bounds)
         let mainController = HomeViewController()
